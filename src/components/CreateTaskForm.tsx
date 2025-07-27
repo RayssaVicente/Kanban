b9c74116f08af3ef82a345e49d0f348716534018
Rayssa Vicente <rayssavicenteviegas@gmail.com>
2025-07-27 11:18:13 -0300

    Adiciona a função responsável por exibir as tasks após a criação com await createTask(taskData)

--- src/components/CreateTaskForm.tsx
+++ src/components/CreateTaskForm.tsx
@@ -2,6 +2,8 @@ import {Dialog,Button, Flex, Box, Text, TextField, TextArea, RadioGroup,Badge} f
 import { PlusIcon } from "@radix-ui/react-icons"
 import { FormEventHandler } from "react"
 import {z} from "zod"
+import { useTasks } from "../hooks/useTasks"
+
 
 const CreateTaskSchema = z.object({
     title: z.string(),
@@ -11,6 +13,8 @@ const CreateTaskSchema = z.object({
 })
 
 export const CreateTaskForm: React.FC = ()=> {
+    const {createTask} = useTasks()
+
     const handleSubmit: FormEventHandler<HTMLFormElement> = async (ev)=>{
         ev.preventDefault()
 
@@ -24,7 +28,7 @@ export const CreateTaskForm: React.FC = ()=> {
         ev.currentTarget.reset()
 
         const taskData = CreateTaskSchema.parse({title, description, status, priority})
-        alert(JSON.stringify(taskData))
+        await createTask(taskData)
     }
     return(
         <Dialog.Root>
