5a335e813709a30ce6d37d70b0d1e5249b222bdd
Rayssa Vicente <rayssavicenteviegas@gmail.com>
2025-07-27 11:22:51 -0300

    Filtrando e exibindo dinamicamente as tasks de acordo com o status (todo, doing, done)

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -1,63 +1,41 @@
 import { Grid, Flex, Badge} from "@radix-ui/themes"
 import { Task } from "../entities/Task"
 import {TaskCard} from "./TaskCard"
+import { useContext } from "react"
+import { TasksContext } from "../contexts/TasksContext"
+//import {useTasks} from "../hooks/useTasks"
 
 export const TaskBoard: React.FC = () => {
-    const tasksTodo: Task[] = [
-        {
-            "id": 2,
-            "title" : "Reunião com as equipes",
-            "description": "Reunião para descutir o progresso do projeto e próximos passos.",
-            "status": "todo",
-            "priority": "high"
-        },
-        {
-            "id": 4,
-            "title": "Implementar testes",
-            "description": "Desenvolver os testes automatizados na nova funcionalidade do aplicativo.",
-            "status": "todo",
-            "priority": "medium"
-        }
-    ]
-    const tasksInProgress: Task[] = [
-        {
-            "id": 1,
-            "title": "Enviar relatorio",
-            "description": "Enviar o relatorio mensal para o departamento financeiro",
-            "status": "doing",
-            "priority": "high"
-        }
-    ]
 
-    const tasksDone: Task[] = [
-        {
-            "id": 3,
-            "title": "Atualizar o site",
-            "description": "Fazer atualizações no site da empresa com novas informações.",
-            "status": "done",
-            "priority": "low"
-        }
-    ]
+    const {tasks} = useContext(TasksContext)
+
+    // const {tasks} = useTasks()
+
+    const tasksTodo: Task[] = tasks.filter(task => task.status === "todo") ?? []
+
+    const tasksInProgress: Task[] = tasks.filter(task => task.status === "doing") ?? []
+
+    const tasksDone: Task[] = tasks.filter(task => task.status === "done") ?? []
 
     return(
         <Grid columns="3" gap="4" minWidth="64rem">
             <Flex direction="column" gap="4">
-                <Badge size="3" color="indigo">
-                    Para Fazer(2)
+                <Badge size="3" color="gray">
+                    Para Fazer({tasksTodo.length})
                 </Badge>
                 {tasksTodo.map((task) => <TaskCard key={task.id} task={task}/>)}
             </Flex>
 
             <Flex direction="column" gap="4">
-                <Badge size="3" color="green">
-                    Em Progresso (2)
+                <Badge size="3" color="yellow">
+                    Em Progresso ({tasksInProgress.length})
                 </Badge>
                 {tasksInProgress.map((task) => <TaskCard key={task.id} task={task}/>)}
             </Flex>
 
             <Flex direction="column" gap="4">
-                <Badge size="3" color="bronze">
-                    Concluido (2)
+                <Badge size="3" color="green">
+                    Concluido ({tasksDone.length})
                 </Badge>
                 {tasksDone.map((task) => <TaskCard key={task.id} task={task}/>)}
             </Flex>
