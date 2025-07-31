7c4b3324801934abaa4aed7348bfb9cc0a6629a0
Rayssa Vicente <rayssavicenteviegas@gmail.com>
2025-07-31 19:15:11 -0300

    Implementa as funções de atualização (PATCH) e exclusão (DELETE) na API de tarefas

--- src/services/api.ts
+++ src/services/api.ts
@@ -21,5 +21,23 @@ export const tasksService = {
         const newTask: Task = await response.json();
         return newTask;
     },
+
+    async updateTask(id: string, attributes: Partial<Omit<Task, "id">>): Promise<Task>{
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/tasks/${id}`, {
+            method: "PATCH",
+            headers: {
+            "Content-Type": "application/json"
+            },
+            body: JSON.stringify(attributes)
+        })
+        const updateTask: Task = await response.json()
+        return updateTask
+    },
+
+    async deleteTask(id: string): Promise<void>{
+        await fetch(`${import.meta.env.VITE_API_URL}/tasks/${id}`, {
+            method : "DELETE"
+        })
+    }
 };
 
